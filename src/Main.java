5bf53d3034c2e4dfa490d97d1d6b81ee1f36d5a5
Tungtungzz <90121922+Tungtungzz@users.noreply.github.com>
2023-11-14 11:43:54 +0700

    1

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if ( isPrime(a) )
+            System.out.print("Yes");
+        else
+            System.out.print("No");
+        }
+    static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
