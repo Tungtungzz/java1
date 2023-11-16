561c4ab2c3114e23aa15fd3dda080d84f90d1d9c
Tungtungzz <90121922+Tungtungzz@users.noreply.github.com>
2023-11-16 08:14:11 +0700

    1

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        int closestPrime = findClosestPrime(a);
+        System.out.println(closestPrime);
 //        if ( isPrime(a) )
 //            System.out.print("Yes");
 //        else
 //            System.out.print("No");
-        System.out.print(fibonacci(a-1));
+//        System.out.print(fibonacci(a-1));
         }
 
     static boolean isPrime(int number) {
@@ -23,20 +25,42 @@ public class Main {
         }
         return true;
     }
-    public static int fibonacci(int n) {
-        if (n <= 1) {
-            return n;
+//    public static int fibonacci(int n) {
+//        if (n <= 1) {
+//            return n;
+//        }
+//
+//        int fib = 1;
+//        int prevFib = 1;
+//
+//        for (int i = 2; i < n; i++) {
+//            int temp = fib;
+//            fib += prevFib;
+//            prevFib = temp;
+//        }
+//
+//        return fib;
+//    }
+
+    public static int findClosestPrime(int n) {
+        if (n <= 2) {
+            return 2; // Số nguyên tố đầu tiên là 2
         }
 
-        int fib = 1;
-        int prevFib = 1;
+        int closestPrime = 0;
+        int difference = Integer.MAX_VALUE;
 
-        for (int i = 2; i < n; i++) {
-            int temp = fib;
-            fib += prevFib;
-            prevFib = temp;
+        for (int i = n - 1; i >= 2; i--) {
+            if (isPrime(i)) {
+                int currentDifference = n - i;
+
+                if (currentDifference < difference) {
+                    closestPrime = i;
+                    difference = currentDifference;
+                }
+            }
         }
 
-        return fib;
+        return closestPrime;
     }
 }
